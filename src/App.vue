7ea795cbcf2278084c9d7b42504899c4c6c48899
Eugeny Gorshkov <gorshkov.zhenya@mail.ru>
2024-05-07 14:01:59 +0300

    feat: first commit, init proj

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container mx-auto">
+    <form action="" class="flex flex-col gap-4 m-10">
+      <input type="text" class="border border-gray-600 max-w-64" placeholder="login...">
+      <input type="text" class="border border-gray-600 max-w-64" placeholder="password...">
+      <div class="flex gap-3">
+        <button class="font-bold border border-gray-600 min-w-24">Login</button>
+        <button class="font-bold border border-gray-600 min-w-24">Register</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+
+<script setup lang="ts">
+
+</script>
